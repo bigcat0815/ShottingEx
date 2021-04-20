d2248fb512123b1944536b68a68f984e1e8feb00
Bigcat0815 <redapply@naver.com>
2021-04-20 23:42:17 +0900

    # 2. 플레이샷 프리펩 - 아직 스크립트 연결 안됨 - 프리펩 강좌 9:35부터 참고 바람

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,37 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 7.0f;
+    
+    //이동제한
+    private Vector3 _min, _max;
+    private Vector2 _coliderHalfSize;
+    
     void Start()
     {
         Managers.Input.OnKeyAction -= OnKeyInput;
         Managers.Input.OnKeyAction += OnKeyInput;
 
-        Vector3 worldPos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
-        print(worldPos);
+        _min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        _max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+         Vector2 boxSize = gameObject.GetComponent<BoxCollider2D>().bounds.size;
+        _coliderHalfSize = new Vector2(boxSize.x/2 , boxSize.y /2);
 
     }
 
     
     void Update()
     {
-        
+           
     }
 
     void OnKeyInput()
     {
-        if (Input.GetKey(KeyCode.W)) { transform.position += Vector3.up * Time.deltaTime * _speed; }
+        /*if (Input.GetKey(KeyCode.W)) { transform.position += Vector3.up * Time.deltaTime * _speed; }
         if (Input.GetKey(KeyCode.S)) { transform.position += Vector3.down * Time.deltaTime * _speed; }
         if (Input.GetKey(KeyCode.A)) { transform.position += Vector3.left  * Time.deltaTime * _speed; }
-        if (Input.GetKey(KeyCode.D)) { transform.position += Vector3.right * Time.deltaTime * _speed; }
+        if (Input.GetKey(KeyCode.D)) { transform.position += Vector3.right * Time.deltaTime * _speed; }*/
+        
+        /*if (Input.GetKey(KeyCode.W)) { transform.Translate(Vector3.up * Time.deltaTime * _speed); }
+        if (Input.GetKey(KeyCode.S)) { transform.Translate(Vector3.down * Time.deltaTime * _speed); }
+        if (Input.GetKey(KeyCode.A)) { transform.Translate(Vector3.left  * Time.deltaTime * _speed); }
+        if (Input.GetKey(KeyCode.D)) { transform.Translate(Vector3.right * Time.deltaTime * _speed); }*/
+        
 
-        /*float x = Input.GetAxisRaw("Horizontal");
+        float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(x, y, 0).normalized;
-        transform.position += dir * Time.deltaTime * _speed;*/
+        transform.position += dir * Time.deltaTime * _speed;
+
+        float newX = Mathf.Clamp(transform.position.x, _min.x + _coliderHalfSize.x +0.5f, _max.x - _coliderHalfSize.x -0.2f);
+        float newY = Mathf.Clamp(transform.position.y, _min.y + _coliderHalfSize.y +0.3f, _max.y - _coliderHalfSize.y -0.2f);
+        transform.position = new Vector3(newX, newY, transform.position.z);
         
     }
 }
