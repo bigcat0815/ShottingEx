b0e393cba840b29edfe7177840646ad7b55fd88a
Bigcat0815 <redapply@naver.com>
2021-04-26 12:01:53 +0900

    # 7 . UI 연결 - 단 Item UI는 앵커가 안맞는건지 무엇때문인지 센터에 맞지 않는다. 그 외에는 잘 연결 되었다.

--- Assets/Scripts/UI/Scene/UI_Coin.cs
+++ Assets/Scripts/UI/Scene/UI_Coin.cs
@@ -8,7 +8,8 @@ public class UI_Coin : UI_Scene
 {
     private Text _coinText;
     private Button _pauseButton;
-
+   
+    
     public Text CoinUIText
     {
         get { return _coinText; }
@@ -24,6 +25,12 @@ public class UI_Coin : UI_Scene
     {
         PauseButton,
     }
+
+    enum GameObjects
+    {
+        //ItemPanel,
+        PausePanel,
+    }
     
     void Start()
     {
@@ -34,6 +41,11 @@ public class UI_Coin : UI_Scene
     {
         base.Init();
 
+        //2. GameObjects - visual만 제어
+        Bind<GameObject>(typeof(GameObjects));
+        GameObject pausePanel = Get<GameObject>((int) GameObjects.PausePanel);
+        pausePanel.SetActive(false);
+        
         //text
         Bind<Text>(typeof(Texts));
         _coinText = GetText((int) Texts.CoinText);
@@ -46,6 +58,18 @@ public class UI_Coin : UI_Scene
         _pauseButton = GetButton((int) Buttons.PauseButton);
         _pauseButton.gameObject.BindEvent(PauseAction,Define.UIEvent.Click);
         
+       
+        
+        //1. GameObjects - subItem 관계가 있을때;
+        //현재 화면이 앵커가 잘 붙지 않아 일단 보류
+        /*Bind<GameObject>(typeof(GameObjects));
+        GameObject itemPanel = Get<GameObject>((int) GameObjects.ItemPanel);
+        foreach (Transform child in itemPanel.transform)
+        {
+            Managers.Resource.Destroy(child.gameObject);
+        }*/
+        
+      
     }
     
     void SetCoinText(int totalCoin)
@@ -55,6 +79,35 @@ public class UI_Coin : UI_Scene
 
     void PauseAction(PointerEventData data)
     {
-        Debug.Log("Pause Action Clicked!!");
+        
+        GameObject pausePanel = Get<GameObject>((int) GameObjects.PausePanel);
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        
+        PausePanel pauseScript = pausePanel.GetOrAddComponent<PausePanel>();
+        
+        
+        /* //1. GameObject item 보류
+        GameObject itemPanel = Get<GameObject>((int) GameObjects.ItemPanel);
+        foreach (Transform child in itemPanel.transform)
+        {
+            Managers.Resource.Destroy(child.gameObject);
+        }
+        
+        GameObject _pauseItem = Managers.UI.MakeSubItem<PausePanel>(itemPanel.transform).gameObject;
+        _pauseItem.transform.SetParent(itemPanel.transform);
+        Time.timeScale = 0;
+        */
+        
+        /*Debug.Log($"itemPanel Pos : {itemPanel.transform.position}");
+        Debug.Log($"_pauseItem Pos : {_pauseItem.transform.position}");
+       _pauseItem.transform.position = new Vector3(0, 0, 0);
+       itemPanel.transform.position = new Vector3(0, 0, 0);*/
+        //_pauseItem.SetActive(true);
+        
+        //PausePanel pauseScript = pauseItem.GetOrAddComponent<PausePanel>();
+
+
     }
+    
 }
---
+++ Assets/Scripts/UI/SubItem/PausePanel.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PausePanel : UI_Base
+{
+    private Button _mainMenuButton;
+    private Button _resumeButton;
+    
+    private string _name;
+    
+    enum Texts
+    {
+        MainMenuText,
+        ResumeText,
+    }
+    
+    enum Buttons
+    {
+        ResumeButton,
+        MainMenuButton
+    }
+    
+    enum GameObjects
+    {
+        PausePanel,
+    }
+
+    void Start()
+    {
+        Init();
+    }
+    
+    public override void Init()
+    {
+        
+        Bind<GameObject>(typeof(GameObjects));
+        GameObject pausePanel = Get<GameObject>((int) GameObjects.PausePanel);
+        
+        Bind<Button>(typeof(Buttons));
+        _resumeButton = GetButton((int) Buttons.ResumeButton);
+        _resumeButton.gameObject.BindEvent( (PointerEventData data)=>
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+            
+        },Define.UIEvent.Click);
+        
+        _mainMenuButton = GetButton((int) Buttons.MainMenuButton);
+        _mainMenuButton.gameObject.BindEvent( (PointerEventData data)=>
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+            
+        },Define.UIEvent.Click);
+        
+        /*Bind<GameObject>(typeof(GameObjects));
+
+        GetGameObject((int) GameObjects.ItemNameText).GetComponent<Text>().text = _name;
+        
+        GetGameObject((int) GameObjects.ItemIcon).AddUIEvent((PointerEventData data) =>
+        {
+            Debug.Log($"아이템 클릭 :{_name}");   
+        });*/
+    }
+
+    public void SetInfo(string name)
+    {
+        _name = name;
+    }
+
+}
